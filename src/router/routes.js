479984d1c824f7a38ca45bfdc9f23053d568d40d
ELIUSXD <114197151+AcuLuis@users.noreply.github>
2024-06-12 23:47:18 -0500

    Se hizo login simple de html

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,22 +1,20 @@
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
   },
   {
-    path: '/principal',
-    component: () => import('components/auth/MainPage.vue')
+    path: "/Login",
+    component: () => import("components/auth/Login.vue"),
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
